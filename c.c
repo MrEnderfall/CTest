c0278058c51e435c2acd72667502846b5b44213e
MrEnderfall <mrenderfall@cocaine.ninja>
2017-09-11 21:45:40 +0300

    ddd

--- c.c
+++ c.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#include <conio.h>
 int main()
 {
-    int num7 = 7;
-    int num6 = 6;
-    if (num7>num6)
-    printf("%d больше %d",num7, num6);
+    int num;
+    int num1;
+    printf("Введите первое число\n");
+        scanf("%ds",num);
+    printf("Введите второе число\n");
+        scanf("%d",num1);
+    if (num>num1)
+    printf("%d больше %d",num, num1);
     else
-        printf("ЛОЖЬ");
+        printf("%d меньше %d",num, num1);
 }
