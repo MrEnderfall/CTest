c61870ebc80e5c8a8bcc8fe617cfd2f388015e24
MrEnderfall <mrenderfall@cocaine.ninja>
2017-09-11 22:26:49 +0300

    whoru.c

---
+++ whoru.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    char me[20];
+    printf('Как ткбя зовут?\n');
+    scanf("%s"&me);
+    printf("Приятно познакомиться, %s\n",me);
+    return(0);
+}
