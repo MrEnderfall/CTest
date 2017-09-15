1764ddcc16d3bde06476d386ccc153b08cc3a81b
MrEnderfall <mrenderfall@cocaine.ninja>
2017-09-15 21:38:05 +0300

    some c++ will not harm, yeah?

---
+++ idk.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main()
+{
+    using namespace std;
+    cout << "IDK";
+    cout << endl;
+    cout << "YOBA ETO TI";
+    return 0;
+}
--- whoru.c
+++ whoru.c
@@ -2,9 +2,9 @@
 #include <conio.h>
 int main()
 {
-    char me[20];
-    printf('Как ткбя зовут?\n');
-    scanf("%s"&me);
+    char*me[20];
+    printf("Как ткбя зовут?\n");
+    scanf(("%s")me);
     printf("Приятно познакомиться, %s\n",me);
     return(0);
 }
