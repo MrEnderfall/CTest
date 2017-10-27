5c54541a0c111af918779f46ea5ed1036e6094b6
MrEnderfall <mrenderfall@cocaine.ninja>
2017-10-27 19:58:40 +0300

    DDD

--- idk.cpp
+++ idk.cpp
@@ -2,8 +2,10 @@
 int main()
 {
     using namespace std;
-    cout << "дурацкий русский! ЮНИКОД ЭДИШОН";
-    cout << endl;
-    cout << "YOBA ETO TI";
+    int smth;
+    cout << "How much NOTHING you have?" << '\n';
+    cin >> smth;
+    smth = smth + 1;
+    cout << "HAHA, I HAVE ONE MORE, I HAVE " << smth << "NOTHINGS";
     return 0;
 }
